7f0b0d20c215c5638bcb356299733126df7e0bd7
Nik Kojevnikov <Nik.Kojevnikov@uhn.ca>
2023-11-23 11:34:01 -0500

    improved algoritm

--- js_fool/script.js
+++ js_fool/script.js
@@ -1,3 +1,36 @@
+let heatMapArr = Array.from({length: 37}, () => Array.from({length: 18}, () => 0))
+let heatMap = 
+   `0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	3	3	3	5	7	9	11	12	13	15	16	17
+    0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	1	3	3	3	5	5	5	7	9	11	12	13	15	16	17	17
+    0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	1	1	3	3	3	3	5	5	5	7	7	7	9	11	12	13	15	16	17	17	17
+    0	0	0	0	0	0	0	1	1	1	1	1	1	3	3	3	3	3	5	5	5	5	7	7	7	9	9	9	11	12	13	15	16	17	17	17	17
+    0	1	1	1	1	1	1	3	3	3	3	3	3	5	5	5	5	5	7	7	7	7	9	9	9	11	11	11	12	13	15	16	17	17	17	17	17
+    1	3	3	3	3	3	3	5	5	5	5	5	5	7	7	7	7	7	9	9	9	9	11	11	11	12	12	12	13	15	16	17	17	17	17	17	17
+    3	5	5	5	5	5	5	7	7	7	7	7	7	9	9	9	9	9	11	11	11	11	12	12	12	13	13	13	15	16	17	17	17	17	17	17	17
+    5	7	7	7	7	7	7	9	9	9	9	9	9	11	11	11	11	11	12	12	12	12	13	13	13	15	15	15	16	17	17	17	17	17	17	17	17
+    7	9	9	9	9	9	9	11	11	11	11	11	11	12	12	12	12	12	13	13	13	13	15	15	15	16	16	16	17	17	17	17	17	17	17	17	17
+    9	11	11	11	11	11	11	12	12	12	12	12	12	13	13	13	13	13	15	15	15	15	16	16	16	17	17	17	17	17	17	17	17	17	17	17	17
+    11	12	12	12	12	12	12	13	13	13	13	13	13	15	15	15	15	15	16	16	16	16	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    12	13	13	13	13	13	13	15	15	15	15	15	15	16	16	16	16	16	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    13	15	15	15	15	15	15	16	16	16	16	16	16	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    15	16	16	16	16	16	16	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    16	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17
+    17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17	17`;
+
+heatMap.split('\n').forEach((row, i) => {
+    let rowArr = row.split('\t');
+    rowArr.forEach((cell, j) => {
+       heatMapArr[36-j][17-i] = parseInt(cell);
+    });
+});
+
+
+
+
+
+
 let deck = [];
 let discard = [];
 let table = [];
@@ -9,8 +42,10 @@ let isPlayerTurn = false;
 let isPlayerMove = false;
 let winner;
 
-let message = '';
+let showPcCards = document.querySelector('#showComputerCards');
 
+
+let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
 // #region - get card graphics from unicode chars
 /*
     0xD83C 0xDCA0 - Playing Card Back
@@ -42,6 +77,16 @@ for (let i = 0; i < 36; i++) {
 }
 // #endregion
 
+let myCard =  {
+    img: 0xDCA2,
+    suit: 0,
+    value: 2,
+    fx: 2,
+    fy: 291,
+    x: 500,
+    y: 250
+};
+
 // #region - drawing
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
@@ -71,6 +116,8 @@ function draw() {
     ctx.fillStyle = bg;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+    
+
     if (isPlayerTurn) {
         drawButton(btnPass);
     } else {
@@ -78,11 +125,12 @@ function draw() {
     }
 
     // message
-    message = winner != undefined ? (winner == p1 ? 'You win!' : 'You lose') : (isPlayerMove ? 'Your turn' : 'Computer turn');
+    let message = winner != undefined ? (winner == p1 ? 'You win!' : 'You lose') : (isPlayerMove ? 'Your turn' : 'Computer turn');
     ctx.save();    
-    ctx.translate(ctx.canvas.width / 2 - (message.length * 3), ctx.canvas.height - 10);
+    ctx.translate(ctx.canvas.width / 2, ctx.canvas.height - 10);
     ctx.font = '1rem sans-serif';
     ctx.fillStyle = 'black';    
+    ctx.textAlign = 'center'
     ctx.fillText(message, 0, 0);
     ctx.restore();
     
@@ -98,8 +146,16 @@ function draw() {
     step = Math.min(40, ctx.canvas.height / p2.length);
     totalHeight = Math.min(ctx.canvas.height, step * p2.length + 80);
     offset = 106 + (ctx.canvas.height - totalHeight) / 2;    
-    for (let i = 0; i < p2.length; i++) {        
-        drawCard(ctx.canvas.width-84, (offset + (step*i)), p2[i]);
+    for (let i = 0; i < p2.length; i++) {  
+        let card = {
+            img: 0xDCA0,
+            suit: 1,
+            value: 0        
+        };      
+        if (showPcCards.checked) {
+            card = p2[i];
+        }
+        drawCard(ctx.canvas.width-84, (offset + (step*i)), card);
     }
 
     // draw trump
@@ -148,6 +204,10 @@ function draw() {
             drawCard(xOffset + (step*j) + (6), yOffset + (ctx.canvas.height / 2) + 34, table[i+1]);
         }
     }
+
+    //let aa = (myCard.fx - myCard.x)/2;
+    //let bb = (myCard.fy - myCard.y)/2;
+    //drawCard(aa,bb, myCard);
     //requestAnimationFrame(draw);
 }
 
@@ -232,6 +292,9 @@ canvas.addEventListener('mouseup', (event) => {
         if (canToss.indexOf(p1[i]) == -1) {
             i = -1;
         }
+    }
+    if (deck.length && !p2.length) {
+        i = -1;
     }
     if (isPlayerMove && i > -1) {
         table.push(...p1.splice(i,1));
@@ -324,6 +387,18 @@ async function computerMove() {
                 
         if (canToss.length) {
             i = p2.indexOf(canToss[0]);
+            
+            if (table.length > 0) {
+                let tossCard = canToss[0];
+                let tossValue = tossCard.value + (tossCard.suit == trump.suit ? 9 : 0);
+                
+                console.log('tossCard:', `${tossCard.value}${suits[tossCard.suit]}`, 'tossValue', tossValue, 'heatMap', heatMapArr[deck.length][tossValue-6]);
+
+                if (heatMapArr[deck.length][tossValue-6] < 16) {                    
+                    console.log('Alorithm says no');
+                    i = -1;
+                }
+            }
         }
 
         if (i > -1) {
@@ -347,6 +422,21 @@ async function computerMove() {
         
         if (canBeat.length) {
             i = p2.indexOf(canBeat[0]);
+            
+            let attachCard = table[table.length-1];
+            let attackValue = attachCard.value + (attachCard.suit == trump.suit ? 9 : 0);
+            let beatCard = canBeat[0];
+            let beatValue = beatCard.value + (beatCard.suit == trump.suit ? 9 : 0);
+            
+            let row = deck.length;
+
+            let diff = beatValue - attackValue;
+            console.log('attack:', `${attachCard.value}${suits[attachCard.suit]}`, 'Beat:', `${beatCard.value}${suits[beatCard.suit]}`, 'diff', diff, 'heatMap', heatMapArr[row][beatValue-6]);
+
+            if (diff > heatMapArr[row][beatValue-6]) {
+                console.log('Alorithm says no');
+                i = -1;
+            }            
         }
 
         if (i > -1) {        
@@ -363,9 +453,9 @@ async function computerMove() {
     draw();
 }
 
-
 (function() {    
     deck = deck.sort(() => 0.5 - Math.random()); // shuffle deck
+
     dealCards(p1, p2);
     
     trump = deck[0];
@@ -384,3 +474,5 @@ async function computerMove() {
     computerMove();    
 })();
 // #endregion
+
+showPcCards.addEventListener('change', () => draw());
